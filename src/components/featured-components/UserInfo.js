ac7657dd88ac27261168bd89fafb531a54dee92c
Jimmy Lam <JimmyLam978@gmail.com>
2020-08-20 15:06:38 -0400

    reload after logout

--- src/components/featured-components/UserInfo.js
+++ src/components/featured-components/UserInfo.js
@@ -17,7 +17,7 @@ const UserInfo = () => {
         axios(`${process.env.REACT_APP_BACK_URI}/logout`, {withCredentials: true})
             .then(response => {
                 console.log(response)
-                //window.location.reload()
+                window.location.reload()
             })
             .catch(error => console.log(error))
     }
